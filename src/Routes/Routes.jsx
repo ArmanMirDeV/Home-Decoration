3cd942373d1e902fb7f84a323de9ba1468c2a3d1
Arman Mir <mirarman8583@gmail.com>
2025-10-06 22:18:31 +0600

    Implement routing with MainLayout and add Home and Products pages; create Footer, Navbar, and LoadSpinner components

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,15 +1,30 @@
 import { createBrowserRouter } from "react-router";
-import App from "../App";
+
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
 
 
 
 
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>
-  }
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+               index: true,
+                element: <Home />
+            },
+
+            {
+                path: "/products",
+                element: <Products />
+            },
+        ]
+    },
+
 ]);
 
-export default router;
+export default router
