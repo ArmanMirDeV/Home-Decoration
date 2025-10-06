c06514abd95139c8328728a2df790dd282a57ea9
Arman Mir <mirarman8583@gmail.com>
2025-10-06 21:59:49 +0600

    Add routing and update dependencies; refactor App component
    - Introduced react-router for routing functionality.
    - Added react-router and daisyui as dependencies.
    - Updated App component to include new button styles and simplified structure.
    - Removed unused CSS styles from App.css.
    - Updated index.css to include daisyui plugin.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from "react-router/dom";
+import router from './Routes/Routes.jsx';
+
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Declarative mode */}
+    {/*  <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App></App>} ></Route>
+      </Routes>
+
+    </BrowserRouter>, */}
+    {/* Data Mode */}
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+
+
+
+
   </StrictMode>,
 )
