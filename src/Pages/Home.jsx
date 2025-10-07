c219bda3c378e88a8692a89102fa9a03c9c1a388
Arman Mir <mirarman8583@gmail.com>
2025-10-07 15:17:10 +0600

    Add useProducts hook for fetching product data and update Home and Products components to utilize it

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router';
+import { Link } from 'react-router';
 import ProductCard from '../Components/ProductCard';
+import useProducts from '../Hooks/useProducts';
 
 const Home = () => {
-    const products = useLoaderData();
+
+    const {products, loading, error} = useProducts()
     const featuredProducts = products.slice(0, 6)
-    console.log(products);
 
     return (
         <div>
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import useProducts from '../Hooks/useProducts';
 
 const Products = () => {
+    const {products} = useProducts()
     return (
         <div>
-            <h1>This is Products</h1>
+            <h1>This is Products: {products.length} </h1>
         </div>
     );
 };
