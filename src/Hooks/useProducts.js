09676ee3c82e259c34376006b02560f3b1ea766e
Arman Mir <mirarman8583@gmail.com>
2025-10-07 15:24:08 +0600

    Refactor Products component layout: enhance UI with header and product grid display

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 
 const useProducts = () =>{
+    
 
     const [products, setProducts] = useState([]);
     const [loading , setLoading] = useState(true);
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import useProducts from '../Hooks/useProducts';
+import ProductCard from '../Components/ProductCard';
+import { Link } from 'react-router';
 
 const Products = () => {
     const {products} = useProducts()
     return (
         <div>
-            <h1>This is Products: {products.length} </h1>
+            <div className='flex justify-between py-5 items-center ' >
+                <h1 className='text-3xl font-semibold ' >All Products</h1>
+                <button className='btn btn-outline text-blue-500' to='/products' >Search</button>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4' >
+                {
+                    products.map(product =>
+                        <ProductCard product={product} key={product.id} ></ProductCard>
+                    )
+                }
+            </div>
         </div>
     );
 };
